e8799b630184cfc1f47fec19d4d4478adc5dc6cd
nduje <dujerancic@gmail.com>
2025-12-14 10:38:39 +0100

    Add seeds

---
+++ seeds/match_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO MatchTypes (MatchType) VALUES ('Group');
+INSERT INTO MatchTypes (MatchType) VALUES ('Round of 16');
+INSERT INTO MatchTypes (MatchType) VALUES ('Quarterfinal');
+INSERT INTO MatchTypes (MatchType) VALUES ('Semifinal');
+INSERT INTO MatchTypes (MatchType) VALUES ('Final');
---
+++ seeds/tournaments.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tournaments (TournamentId, Name, Year, Location, WinnerTeamId) VALUES (1, 'FIFA World Cup 2022', 2022, 'Qatar', NULL);
+INSERT INTO Tournaments (TournamentId, Name, Year, Location, WinnerTeamId) VALUES (2, 'UEFA Euro 2020', 2020, 'Europe', NULL);
+INSERT INTO Tournaments (TournamentId, Name, Year, Location, WinnerTeamId) VALUES (3, 'Copa América 2021', 2021, 'Brazil', NULL);
+INSERT INTO Tournaments (TournamentId, Name, Year, Location, WinnerTeamId) VALUES (4, 'UEFA Champions League', 2023, 'Europe', NULL);
+INSERT INTO Tournaments (TournamentId, Name, Year, Location, WinnerTeamId) VALUES (5, 'CONCACAF Gold Cup', 2023, 'North and Central America', NULL);
